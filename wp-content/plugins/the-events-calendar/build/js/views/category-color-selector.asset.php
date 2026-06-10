07429f0ceabd2c040c80e5726b33332d5cbaa2a0
Andrew DeFaria <Andrew@DeFaria.com>
2026-06-10 14:32:27 -0700

    Maintenance: Remove third-party plugins and themes from git tracking

--- wp-content/plugins/the-events-calendar/build/js/views/category-color-selector.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '52b904c19ce12a16e6e9');
--- wp-content/plugins/the-events-calendar/common/vendor/vendor-prefixed/stellarwp/harbor/src/Harbor/View/Exceptions/FileNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php declare( strict_types=1 );
-
-namespace TEC\Common\LiquidWeb\Harbor\View\Exceptions;
-
-use Exception;
-
-final class FileNotFoundException extends Exception {
-
-}
